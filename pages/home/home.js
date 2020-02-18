84dc0ad801207e11bf977716c3d9f3ebcd9a8fd9
zhailianxing <575206613@qq.com>
2020-02-18 13:46:46 +0800

    修复轮播图样式不兼容iphone6等手机；推荐icon展示

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,23 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-      banner:[]
+    banner: [],
+    recommend:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-      wx.request({
-        url: 'http://123.207.32.32:8000/home/multidata',
-        success:(res)=> {
-          console.log(res)
-          if (res.statusCode == 200){
-            this.setData({
-              banner: res.data.data.banner.list
-            })
-          }
+  onLoad: function(options) {
+    wx.request({
+      url: 'http://123.207.32.32:8000/home/multidata',
+      success: (res) => {
+        console.log(res)
+        if (res.statusCode == 200) {
+          // 设置banner数据
+          this.setData({
+            banner: res.data.data.banner.list
+          })
+          // 设置推荐icon数据
+          this.setData({
+            recommend: res.data.data.recommend.list
+          })
         }
-      })
+      }
+    })
   },
 })
