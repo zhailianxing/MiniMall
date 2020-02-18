51c0abc51abf1e92e4ab9fb187df518c8e51b012
zhailianxing <575206613@qq.com>
2020-02-18 12:48:54 +0800

    首页轮播图封装成组件,因为商品详情页也会用到轮播图

---
+++ components/z-swiper/z-swiper.js
@@ -0,0 +1,37 @@
+// components/z-swiper/z-swiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    list:{
+      type:Array,
+      value:[]
+    },
+
+    interval:{
+      type:Number,
+      value:2500,
+    },
+
+    activeColor:{
+      type:String,
+      value:"#ff5777"
+    }
+   
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
